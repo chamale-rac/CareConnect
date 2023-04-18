f39aec0a4f80896ba4dfa048ceb487568c94d3c8
chamale-rac <samuel.chamale23@gmail.com>
2023-04-18 14:53:08 -0600

    Revised version all working

--- care-connect-app/src/AuthApp.jsx
+++ care-connect-app/src/AuthApp.jsx
@@ -1,81 +1,77 @@
-import React, { useContext } from "react";
-import { UserProvider } from "./context/UserContext";
+import React, { useContext } from 'react'
+import { UserProvider } from './context/UserContext'
 
-import { Link, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import Stock from "./Pages/Stock";
-import Notifications from "./Pages/Notifications";
-import Doctors from "./Pages/Doctors";
-import Landing from "./Pages/Landing";
-import SignIn from "./Pages/SignIn";
-import Login from "./Pages/Login";
-import { NewConsultation } from "./Pages/NewConsultation/NewConsultation";
-import { NewPatient } from "./Pages//NewPatient/NewPatient";
-import { AddProduct } from "./Pages/AddProduct/AddProduct";
-import { ManageStock } from "./Pages/ManageStock/ManageStock";
-import { ManagePatient } from "./Pages/ManagePatient/ManagePatient";
-
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Stock from './Pages/Stock'
+import Notifications from './Pages/Notifications'
+import Landing from './Pages/Landing'
+import SignIn from './Pages/SignIn'
+import Login from './Pages/Login'
+import { NewConsultation } from './Pages/NewConsultation/NewConsultation'
+import { NewPatient } from './Pages//NewPatient/NewPatient'
+import { AddProduct } from './Pages/AddProduct/AddProduct'
+import { ManageStock } from './Pages/ManageStock/ManageStock'
+import { ManagePatient } from './Pages/ManagePatient/ManagePatient'
 
 import PatientProfile from './Pages/Patient/PatientProfile'
 import Consulta from './Pages/Consulta'
-import { ManagePatient} from './Pages/ManagePatient/ManagePatient'
 
 const AuthApp = () => {
-    return (
-        <>
-            <nav>
-                <ul className="list-group">
-                    AUTH APP
-                    <li className="list-group-item">
-                        This is supposed to be the navbar cause it doesnt
-                        re-render
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/stock">Consultas Stock</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/notifications">Notifications</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/doctors">Busqueda de Medicos</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/add-product">Agregar Producto</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/manage-stock">Gestionar Inventario</Link>
-                    </li>
-                </ul>
-            </nav>
-            <UserProvider>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/signin" element={<SignIn />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/stock" element={<Stock />} />
-                    <Route path="/notifications" element={<Notifications />} />
-                    <Route path="/doctors" element={<Doctors />} />
-                    <Route
-                        path="/new-consultation"
-                        element={<NewConsultation />}
-                    />
-                    <Route path="/new-patient" element={<NewPatient />} />
-                    <Route path="/add-product" element={<AddProduct />} />
-                    <Route path="/manage-stock" element={<ManageStock />} />
-                    <Route path="/manage-patient" element={<ManagePatient />} />
-                    <Route path="*" element={<h1>404 Not Found</h1>} />                    
-                    <Route
-                      path="/patient_profile/:patientId"
-                      element={<PatientProfile />}
-                    />
-                    <Route path="/consulta/:consultaId" element={<Consulta />} />
-                </Routes>
-            </UserProvider>
-        </>
-    );
-};
+	return (
+		<>
+			<nav>
+				<ul className="list-group">
+					AUTH APP
+					<li className="list-group-item">
+						This is supposed to be the navbar cause it doesnt
+						re-render
+					</li>
+					<li className="list-group-item">
+						<Link to="/">Home</Link>
+					</li>
+					<li className="list-group-item">
+						<Link to="/stock">Consultas Stock</Link>
+					</li>
+					<li className="list-group-item">
+						<Link to="/notifications">Notifications</Link>
+					</li>
+					<li className="list-group-item">
+						<Link to="/add-product">Agregar Producto</Link>
+					</li>
+					<li className="list-group-item">
+						<Link to="/manage-stock">Gestionar Inventario</Link>
+					</li>
+				</ul>
+			</nav>
+			<UserProvider>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/signin" element={<SignIn />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/stock" element={<Stock />} />
+					<Route path="/notifications" element={<Notifications />} />
+					<Route
+						path="/new-consultation/:patientId?"
+						element={<NewConsultation />}
+					/>
+					<Route path="/new-patient" element={<NewPatient />} />
+					<Route path="/add-product" element={<AddProduct />} />
+					<Route path="/manage-stock" element={<ManageStock />} />
+					<Route path="/manage-patient" element={<ManagePatient />} />
+					<Route path="*" element={<h1>404 Not Found</h1>} />
+					<Route
+						path="/patient_profile/:patientId"
+						element={<PatientProfile />}
+					/>
+					<Route
+						path="/consulta/:consultaId"
+						element={<Consulta />}
+					/>
+				</Routes>
+			</UserProvider>
+		</>
+	)
+}
 
-export default AuthApp;
+export default AuthApp
